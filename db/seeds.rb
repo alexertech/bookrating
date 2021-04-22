a2ec95a9c440e01a4c1dbaef6e33741ba8377b78
Alex B <alexertech@offensive-security.com>
2021-04-21 21:07:18 -0400

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+books = Book.create([
+    {
+        name: "Moby Dick",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327940656l/153747.jpg"
+    },
+    {
+        name: "Monte Cristo",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1611834134l/7126.jpg"
+    },
+    {
+        name: "Minecraft the Island",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1504003184l/32736561._SY475_.jpg"
+    },
+    {
+        name: "Sapiens: A Brief History of Humankind ",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1595674533l/23692271._SY475_.jpg"
+    }
+])
+
+reviews = Review.create([
+    {
+        title: 'Great Book',
+        description: 'One of the first books I read as child, completely love it',
+        score:5,
+        book: books.first
+    },
+    {
+        title: 'Awesome!',
+        description: 'This should be used in the history classes all over the world!',
+        score:5,
+        book: books.last
+    }
+])
