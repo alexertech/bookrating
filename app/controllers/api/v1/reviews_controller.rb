e502132563782ca7d9a9e6d5ae3dc744e8e28a42
Alex B <alexertech@offensive-security.com>
2021-05-03 15:40:33 -0400

    reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,32 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
+
+            def create
+                review = Review.new(review_params)
+            
+                if review.save
+                    render josn: ReviewSerializer.new(review).serialized_json
+                else
+                    render json: { error: review.errors.messages }, status: 422
+                    
+                end
+            end
+
+            def destroy
+                review = Review.new(params[:id])
+            
+                if review.destroy
+                    head :no_content
+                else
+                    render json: { error: review.errors.messages }, status: 422                    
+                end
+            end
+
+            private
+            def review_params
+                params.require(:review).permit(:title, :description, :scoire, :book_id)
+            end
         end
     end
 end
