fb7de790cf456604a8fc09026bbb2fd00f2677d7
Alex B <alexertech@offensive-security.com>
2021-05-05 16:13:05 -0400

    Books listing

--- app/javascript/components/Books/Books.js
+++ app/javascript/components/Books/Books.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Books = () => {
-    return <div>Index view of the app</div>
+
+    const [books, setBooks] = useState([])
+
+    useEffect(()=>{
+        // Get all the books
+        // update state
+        axios.get('/api/v1/books.json')
+        .then( resp => {
+            setBooks( resp.data.data )
+        } )
+        .catch( resp => console.log(resp) )
+    }, [books.length])
+
+    const list = books.map( item=> {
+        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
+
+    return (
+        <Fragment>
+            <div>List of the Books#index</div>
+            <ul>{list}</ul>
+        </Fragment>
+    )
+
 }
 
 export default Books
